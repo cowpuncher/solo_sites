cab51c1db18c37a1ad986b60f00b7b468f34db36
cowboy <tvleanjohnny@gmail.com>
2020-08-06 19:05:42 +0300

    add shadow

--- Sos cafe/js/script.js
+++ Sos cafe/js/script.js
@@ -172,6 +172,8 @@ $(document).ready(function(){
                 $(e).slick({
                     infinite: true,
                     slidesToShow: index,
+                    prevArrow: '<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" fill="#fcd0c4"/></svg></div>',
+                    nextArrow: '<div class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" fill="#fcd0c4"/></svg></div>',
                     slidesToScroll: 1
                 });
             } else if($(e).find(item).length < 2) {
